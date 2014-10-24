2685c0048b930977a4ce6e5d19d5c6e833b2de87
Harry <huangharry19@gmail.com>
2014-10-24 09:38:20 +0800

    add a multiply function

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-int add(int a,int b){
+int add(int a, int b){
  return a+b;
 }
 int subtract(int a, int b){
 return a-b;
 }
+int multiply(int a, int b){
+return a*b;
+}
 void main(){
 printf("hello Git\n");
 printf("a+b=%d\n",add(10,20));
 printf("a-b=%d\n",subtract(20,10));
+printf("a*b=%d\n",multiply(10,20));
 }
 
 
