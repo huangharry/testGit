899f9a42da1e773aade4b5cd615ebc3dd08fa713
Harry <huangharry19@gmail.com>
2014-10-24 09:42:57 +0800

    add a divide function

--- main.c
+++ main.c
@@ -8,11 +8,15 @@ return a-b;
 int multiply(int a, int b){
 return a*b;
 }
+int divide(int a, int b){
+return a/b;
+}
 void main(){
 printf("hello Git\n");
 printf("a+b=%d\n",add(10,20));
 printf("a-b=%d\n",subtract(20,10));
 printf("a*b=%d\n",multiply(10,20));
+printf("a/b=%d\n",divide(20,10));
 }
 
 
